992af8b16b3db654ac12724d1b5e18699a1e5e58
hieu16129 <infor.duchieu.1999@gmail.com>
2023-10-09 08:27:07 +0700

    update

--- src/main/java/com/booking_house_be/controller/AccountController.java
+++ src/main/java/com/booking_house_be/controller/AccountController.java
@@ -66,12 +66,6 @@ public class AccountController {
     @PostMapping("/registerOwner/{id}")
     public Account registerOwner(@PathVariable int id , @RequestBody Account accountEdit) {
         Account account = accountService.getById(id);
-        account.setFirstname(accountEdit.getFirstname());
-        account.setLastname(accountEdit.getLastname());
-        account.setAddress(accountEdit.getAddress());
-        account.setEmail(accountEdit.getEmail());
-        account.setPhone(accountEdit.getPhone());
-        account.setAvatar(accountEdit.getAvatar());
         account.setFrontside(accountEdit.getFrontside());
         account.setBackside(accountEdit.getBackside());
         accountService.edit(account);
