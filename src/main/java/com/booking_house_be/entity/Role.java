9eb0519cfac5afa685f73f301a77dcc63e69340c
Hao <nguyenduchao95@gmail.com>
2023-10-25 10:54:27 +0700

    init data deploy

--- src/main/java/com/booking_house_be/entity/Role.java
+++ src/main/java/com/booking_house_be/entity/Role.java
@@ -1,6 +1,7 @@
 package com.booking_house_be.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
@@ -10,11 +11,16 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+    public Role(String name){
+        this.name = name;
+    }
 
     @Override
     public String getAuthority() {
